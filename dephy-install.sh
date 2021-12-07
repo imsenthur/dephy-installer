61abfb8d240805ec5c85c7ecf54ab0414da6b90d
Senthur Raj <imsenthur@gmail.com>
2021-12-07 15:56:10 -0500

    Added installer script.

---
+++ dephy-install.sh
@@ -0,0 +1,7 @@
+sudo apt-get install python3-pip
+sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
+sudo apt-get install ufw python-scipy libatlas-base-dev -y 
+sudo ufw --force enable && sudo ufw allow 8988 && sudo ufw allow 22
+sudo usermod -a -G dialout "$USER"
+python3 -m pip install --upgrade pip
+python3 -m pip install flexsea
