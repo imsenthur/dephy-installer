009fd5bf25b84830eaaae70629f0a93499f4c00c
Senthur Raj <imsenthur@gmail.com>
2021-12-07 15:59:22 -0500

    Added Dephy repository.

--- dephy-install.sh
+++ dephy-install.sh
@@ -1,3 +1,4 @@
+git clone https://github.com/DephyInc/Actuator-Package.git
 sudo apt-get install python3-pip
 sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
 sudo apt-get install ufw python3-scipy libatlas-base-dev -y 
